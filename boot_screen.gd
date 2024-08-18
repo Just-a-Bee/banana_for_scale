e2fa827dd1104cbcf20926778cb8e8a94ca610ce
Abby Smith <asillylittlecreature@gmail.com>
2024-08-18 14:28:44 -0400

    Added boot screen scene Displays text over time

---
+++ boot_screen.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@onready var title = $PanelContainer/VBoxContainer/Title
+@onready var left_column = $PanelContainer/VBoxContainer/HBoxContainer/LeftColumn
+@onready var right_column = $PanelContainer/VBoxContainer/HBoxContainer/RightColumn
+
+func _ready():
+	show_text()
+
+func show_text():
+	while title.visible_ratio < 1:
+		title.visible_characters += 1
+		await get_tree().create_timer(.05).timeout
+	
+	await get_tree().create_timer(.5).timeout
+	
+	
+	while left_column.visible_ratio < 1:
+		left_column.visible_characters += 1
+		await get_tree().create_timer(.01).timeout
+		
+		if left_column.text[left_column.visible_characters-1] == "\n":
+			await get_tree().create_timer(.2).timeout
+			var right_line_finished = false
+			while not right_line_finished:
+				right_column.visible_characters += 1
+				await get_tree().create_timer(.01).timeout
+				if right_column.text[right_column.visible_characters] == "\n":
+					right_line_finished = true
