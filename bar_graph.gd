025cd152b876de76a7dcbcda5e883e228d26111e
Abby Smith <asillylittlecreature@gmail.com>
2024-08-19 15:55:00 -0400

    Added a lot of new pictures, as well as additional banana assets Also added bargraph for displaying score Added score autoload

---
+++ bar_graph.gd
@@ -0,0 +1,19 @@
+extends VBoxContainer
+
+var BAR_MAX = 160
+
+func _ready():
+	Score.score_changed.connect(update_graph)
+
+func update_graph():
+	var total = 0
+	var max = 0
+	for i in Score.score_array:
+		total += i
+		if i > max:
+			max = i
+	var bars = get_children()
+	bars.reverse()
+	for i in bars.size():
+		bars[i].get_node("Count").text = str(Score.score_array[i])
+		bars[i].get_node("Bar").custom_minimum_size.x = float(Score.score_array[i])/max*BAR_MAX
---
+++ score.gd
@@ -0,0 +1,20 @@
+# Script for autoload to handle score
+# Keeps track of score and emits a signal when changed
+
+extends Node
+
+signal score_changed
+
+var score_array:Array[int] = [0,0,0]
+
+enum SCORES {
+	BAD = 0,
+	GOOD = 1,
+	PERFECT = 2
+}
+
+# function to increment the score at a given index
+func increment(index):
+	score_array[index] += 1
+	score_changed.emit()
+
