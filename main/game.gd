3bde07c691cfca90d449d19f91f086ddfe5430cc
Abby Smith <asillylittlecreature@gmail.com>
2024-08-19 22:16:25 -0400

    Redid game ui to look prettier added aduio dialogue events to "event_queue" - previously picture_queue when a dialogue event is reached, the dialogue is played and the next event is called

--- main/game.gd
+++ main/game.gd
@@ -6,37 +6,58 @@ extends Control
 const BANANA_START_POS = Vector2(48,96)
 
 var current_picture:Picture = null
-var banana_packed = preload("res://banana.tscn")
+var banana_packed = preload("res://banana/banana.tscn")
 
-@onready var picture_rect = $GamePanel/MarginContainer/VBoxContainer/HBoxContainer/PicturePanel/MarginContainer/PictureRect
-@onready var banana = $Banana
+@onready var picture_rect = $GamePanel/VBoxContainer/MarginContainer/VBoxContainer/HBoxContainer/PicturePanel/MarginContainer/PictureRect
+var banana:Banana
 
-@onready var picture_array:Array[Picture] = [
-	Pictures.final_picture,
+@onready var event_array:Array = [
+	preload("res://assets/audio/voice/tutorial.ogg"),
 	Pictures.tutorial_picture,
 	Pictures.get_picture(),
 	Pictures.get_picture(),
 	Pictures.get_picture(),
+	"bad_banana",
 	Pictures.get_picture(),
 	Pictures.get_picture(),
-	
+	preload("res://assets/audio/voice/going_under.ogg"),
+	Pictures.final_picture,
 ]
 
 func _ready():
-	next_picture()
+	banana = banana_packed.instantiate()
+	add_child(banana)
+	banana.move(BANANA_START_POS)
+	next_event()
 
 # Function to display the next picture for banana scaling
-func next_picture():
-	current_picture = picture_array.pop_front()
-	picture_rect.texture = current_picture.texture
-
+func next_event():
+	if event_array.size() == 0:
+		return
+	var event = event_array.pop_front()
+	if event is AudioStream:
+		$Dialogue.stream = event
+		$Dialogue.play()
+		next_event()
+	elif event is Picture:
+		current_picture = event
+		picture_rect.texture = current_picture.texture
+		
+	elif event == "bad_banana":
+		pass
+	
 func _on_button_button_up():
 	score_banana()
-	next_picture()
-	banana.queue_free()
+	next_event()
+	spawn_banana()
+
+
+func spawn_banana():
+	if banana:
+		banana.queue_free()
 	banana = banana_packed.instantiate()
-	banana.position = BANANA_START_POS
 	add_child(banana)
+	banana.move(BANANA_START_POS)
 
 func score_banana():
 	print(banana.get_banana_size())
