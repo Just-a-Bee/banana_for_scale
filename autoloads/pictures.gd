8d5adeceef7702b14f87b0d82f2e48cda5be7643
Isaiah Sugar <readingmaniac2004@gmail.com>
2024-08-20 11:01:31 -0400

    change images to webp and make them smaller

--- autoloads/pictures.gd
+++ autoloads/pictures.gd
@@ -3,21 +3,21 @@
 
 extends Node
 
-var tutorial_picture:Picture = Picture.new(preload("res://assets/pictures/tape_measure.jpg"), 292)
+var tutorial_picture:Picture = Picture.new(preload("res://assets/pictures/tape_measure.webp"), 292)
 
 var picture_arr:Array[Picture] = [
-	Picture.new(preload("res://assets/pictures/still_life.jpg"), 237),
-	Picture.new(preload("res://assets/pictures/table.jpg"), 46),
-	Picture.new(preload("res://assets/pictures/shelf.jpg"), 271),
-	Picture.new(preload("res://assets/pictures/orange_cat.jpg"), 128),
-	Picture.new(preload("res://assets/pictures/tabby_cat.jpg"), 376),
-	Picture.new(preload("res://assets/pictures/nerd.jpg"), 208),
-	Picture.new(preload("res://assets/pictures/helmet.jpg"), 216),
-	Picture.new(preload("res://assets/pictures/shoes.jpg"), 240),
-	Picture.new(preload("res://assets/pictures/vase.jpg"), 375)
+	Picture.new(preload("res://assets/pictures/still_life.webp"), 237),
+	Picture.new(preload("res://assets/pictures/table.webp"), 46),
+	Picture.new(preload("res://assets/pictures/shelf.webp"), 271),
+	Picture.new(preload("res://assets/pictures/orange_cat.webp"), 128),
+	Picture.new(preload("res://assets/pictures/tabby_cat.webp"), 376),
+	Picture.new(preload("res://assets/pictures/nerd.webp"), 208),
+	Picture.new(preload("res://assets/pictures/helmet.webp"), 216),
+	Picture.new(preload("res://assets/pictures/shoes.webp"), 240),
+	Picture.new(preload("res://assets/pictures/vase.webp"), 375)
 ]
 
-var final_picture:Picture = Picture.new(preload("res://assets/pictures/coins.jpg"), 1)
+var final_picture:Picture = Picture.new(preload("res://assets/pictures/coins.webp"), 1)
 
 # function to return a random picture
 func get_picture()->Picture:
